ff547ea6b329c6cce93bcbf8c9f53056a8c15c28
Rajasegar <rajasegar.c@gmail.com>
2021-08-12 12:37:15 +0530

    chore: refactor web.lisp

--- src/web.lisp
+++ src/web.lisp
@@ -18,10 +18,8 @@
 (defvar *web* (make-instance '<web>))
 (clear-routing-rules *web*)
 
-(defvar *day-names* '("Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" ))
-(defvar *month-names* '("January" "February" "March" "April" "May" "June" "July" "August" "September"  "October" "November" "December"))
-
 (defun get-param (name parsed)
+  "Get parameter values from _parsed"
   (cdr (assoc name parsed :test #'string=)))
 
 (defun get-calendar-rows (month year)
@@ -30,7 +28,7 @@
 	 (first-week-days (- 7 first-day))
 	 (remaining-days (- days-in-month first-week-days))
 	 (rows-needed (ceiling (/ remaining-days 7)))
-	 (calendar (make-array 5 :initial-element (make-array 7 :initial-element ""))))
+	 (calendar (make-array (1+ rows-needed) :initial-element (make-array 7 :initial-element ""))))
 
     ;; populate first week
     (let ((first-row (make-array 7 :initial-element "")))
@@ -40,14 +38,13 @@
       (setf (aref calendar 0) first-row))
 
       ;; populate the remaining rows
-      (loop for i from 1 to (1- rows-needed)
+      (loop for i from 1 to rows-needed 
 	    do(let ((temp (make-array 7 :initial-element "")))
 		(loop for j from 0 to 6
 		      do(let ((dt (+ first-week-days (* 7 (- i 1)) (+ j 1))))
 			  (if (<= dt days-in-month)
 			      (setf (aref temp j) dt)
-			      (setf (aref temp j) ""))
-			  ))
+			      (setf (aref temp j) ""))))
 		(setf (aref calendar i) temp)))
       calendar))
 
@@ -60,80 +57,54 @@
 (defun get-current-date ()
   (parse-integer (local-time:format-timestring nil (local-time:now) :format (list :day))))
 
+(defun get-template-data (month year)
+  "Format the data to be sent to Djula templates for calendar routes"
+  (list
+   :day-names local-time:+day-names+
+   :rows (get-calendar-rows month year)
+   :next-month (1+ month)
+   :prev-month (1- month)
+   :year year
+   :month-name (aref local-time:+month-names+ month)
+   :month month
+   :current-date (get-current-date)
+   :current-month (get-current-month)
+   :current-year (get-current-year)))
 ;;
 ;; Routing rules
 
 (defroute "/" ()
   (let* ((month (get-current-month))
 	 (year (get-current-year)))
-  (render #P"index.html" (list
-			  :day-names *day-names*
-			  :rows (get-calendar-rows month year)
-			  :next-month (1+ month)
-			  :prev-month (1- month)
-			  :year year
-			  :month-name (nth (1- month) *month-names*)
-			  :month month
-			  :current-date (get-current-date)
-			  :current-month (get-current-month)
-			  :current-year (get-current-year)))))
+  (render #P"index.html" (get-template-data month year))))
 
 
 (defroute "/next" (&key _parsed)
-  (let* ((month (parse-integer (cdr (assoc "month" _parsed :test #'string=))))
-	(year (parse-integer (cdr (assoc "year" _parsed :test #'string=)))))
+  (let* ((month (parse-integer (get-param "month" _parsed)))
+	(year (parse-integer (get-param "year" _parsed))))
     (if (> month 12)
 	(progn 
 	  (setf month 1)
-	  (setf year (1+ year)))
+	  (incf year))
 	nil)
-  (render #P"_calendar.html" (list
-			  :day-names *day-names*
-			  :rows (get-calendar-rows month year)
-			  :next-month (1+ month)
-			  :prev-month (1- month)
-			  :year year
-			  :month-name (nth (1- month) *month-names*)
-			  :month month
-			  :current-date (get-current-date)
-			  :current-month (get-current-month)
-			  :current-year (get-current-year)))))
+  (render #P"_calendar.html" (get-template-data month year))))
 
 (defroute "/previous" (&key _parsed)
   (print _parsed)
-  (let* ((month (parse-integer (cdr (assoc "month" _parsed :test #'string=))))
-	(year (parse-integer (cdr (assoc "year" _parsed :test #'string=)))))
+  (let* ((month (parse-integer (get-param "month" _parsed)))
+	(year (parse-integer (get-param "year" _parsed))))
     (if (< month 1)
 	(progn
 	  (setf month 12)
-	  (setf year (1- year)))
+	  (decf year))
 	nil)
-  (render #P"_calendar.html" (list
-			  :day-names *day-names*
-			  :rows (get-calendar-rows month year)
-			  :next-month (1+ month)
-			  :prev-month (1- month)
-			  :year year
-			  :month-name (nth (1- month) *month-names*)
-			  :month month
-			  :current-date (get-current-date)
-			  :current-month (get-current-month)
-			  :current-year (get-current-year)))))
+  (render #P"_calendar.html" (get-template-data month year))))
+
 ;; Today
 (defroute "/today" ()
-  (let* ((month (parse-integer (local-time:format-timestring nil (local-time:now) :format (list :month))))
-	 (year (parse-integer (local-time:format-timestring nil (local-time:now) :format (list :year)))))
-  (render #P"_calendar.html" (list
-			  :day-names *day-names*
-			  :rows (get-calendar-rows month year)
-			  :next-month (1+ month)
-			  :prev-month (1- month)
-			  :year year
-			  :month-name (nth (1- month) *month-names*)
-			  :month month
-			  :current-date (get-current-date)
-			  :current-month (get-current-month)
-			  :current-year (get-current-year)))))
+  (let* ((month (get-current-month))
+	 (year (get-current-year)))
+  (render #P"_calendar.html" (get-template-data month year))))
 
 ;; Modal
 (defroute "/modal" (&key _parsed)
@@ -143,27 +114,22 @@
     (render #P"_modal.html" (list :date date
 				  :month month
 				  :year year
-				  :month-name (nth (1- month) *month-names*)))))
+				  :month-name (aref local-time:+month-names+ month)))))
 
 ;; Events
 (defroute ("/events" :method :POST) (&key _parsed)
-  (print _parsed)
-  (let (
-	(date (parse-integer (get-param "date" _parsed)))
+  (let ((date (parse-integer (get-param "date" _parsed)))
 	(month (parse-integer (get-param "month" _parsed)))
 	(year (parse-integer (get-param "year" _parsed)))
 	(time  (get-param "time" _parsed))
 	(ampm  (get-param "ampm" _parsed))
-	(description (get-param "description" _parsed))
-	)
+	(description (get-param "description" _parsed)))
   (render #P"_event.html" (list :date date
 				:month month
 				:year year
 				:time time
 				:ampm ampm
 				:description description))))
-
-
 
 ;;
 ;; Error pages
